fc23818c936c1837110146057f5f5ba6f1b899de
Fabio Kusaba <fabinhokb44@gmail.com>
2023-09-26 20:38:46 -0300

    add: cálculo de conversão, validações de campo

--- app/src/main/java/com/fabiokusaba/conversortemperatura/MainActivity.kt
+++ app/src/main/java/com/fabiokusaba/conversortemperatura/MainActivity.kt
@@ -2,10 +2,24 @@ package com.fabiokusaba.conversortemperatura
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.fabiokusaba.conversortemperatura.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonConverter.setOnClickListener {
+            if (!binding.editCelsius.text.toString().isEmpty()) {
+                val celsius = binding.editCelsius.text.toString().toDouble()
+                val fahrenheit = String.format("%.2f", celsius * 1.8 + 32)
+
+                binding.textResultado.text = "$fahrenheit ºF"
+            } else {
+                binding.textResultado.text = "Temperatura inválida"
+            }
+        }
     }
 }
